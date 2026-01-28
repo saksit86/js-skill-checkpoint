cb35abb8329703bbc06ce7e7b292a48ffc4234f7
Saksit Yimyam <saksit.yimyam86@gmail.com>
2026-01-28 16:58:34 +0700

    Submit Final Assignment

--- question-8.js
+++ question-8.js
@@ -1,6 +1,13 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
-const userList = () => {
-    return fetch("https://jsonplaceholder.typicode.com/users");
+let userList = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    return data
 };
+
+const data = await userList();
+console.log(data)
+const newUsers = data.map(user => user.name)
+console.log(newUsers)
